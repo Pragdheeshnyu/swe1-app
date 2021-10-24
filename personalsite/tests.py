3178f7b1c8e2fcce9b00de7905cf579f4640377a
Pragdheeshnyu <71206520+Pragdheeshnyu@users.noreply.github.com>
2021-10-24 18:02:56 -0400

    Update tests.py

--- personalsite/tests.py
+++ personalsite/tests.py
@@ -1,12 +1,7 @@
 from django.test import TestCase
-import datetime
-from django.utils import timezone
-from django.urls import reverse
 
 
 class QuestionIndexViewTests(TestCase):
     def test_html_admin(self):
         response = self.client.get("/admin")
         self.assertEqual(response.status_code, 301)
-
-    # def test_html_
