8a2177e7f90f663b6c3c606543a4a7c2b4fbdf41
Pragdheeshnyu <71206520+Pragdheeshnyu@users.noreply.github.com>
2021-10-24 17:58:00 -0400

    Deleted packaging and added more test cases

--- polls/tests.py
+++ polls/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 import datetime
 from django.utils import timezone
-from .models import Question
+from .models import Question, Choice
 from django.urls import reverse
 
 # Create your tests here.
@@ -12,7 +12,47 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
+def create_choice(choice_text, question_id):
+    return Choice.objects.create(question=question_id, choice_text=choice_text)
+
+
 class QuestionIndexViewTests(TestCase):
+    def test_html_home_page(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_html_detail_page(self):
+        past_question = create_question(question_text="Present Question.", days=0)
+        response = self.client.get(reverse("polls:detail", args=(past_question.id,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_html_result_page(self):
+        past_question = create_question(question_text="Present Question.", days=0)
+        text = past_question.__str__()
+        time = past_question.was_published_recently()
+        self.assertEqual(text, "Present Question.")
+        self.assertEqual(time, True)
+
+    def test_html_vote_page(self):
+        present_question = create_question(question_text="Present Question.", days=0)
+        response = self.client.get(reverse("polls:vote", args=(present_question.id,)))
+        self.assertEqual(response.status_code, 200)
+        present_choice = create_choice(
+            question_id=present_question, choice_text="Present Choice"
+        )
+        text = present_choice.__str__()
+        self.assertEqual(text, "Present Choice")
+        response = self.client.post(
+            reverse("polls:vote", args=(present_question.id,)),
+            {"choice": str(present_choice.id)},
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_text(self):
+        past_question = create_question(question_text="Past Question.", days=0)
+        response = self.client.get(reverse("polls:results", args=(past_question.id,)))
+        self.assertEqual(response.status_code, 200)
+
     def test_no_questions(self):
         response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code, 200)
